0ea4db868d0c5da86e034ddf8a39fb71855efd4e
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2023-06-24 11:55:21 +0200

    bump dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,19 +5,19 @@ object Versions {
     const val minSdk = 28
     const val targetSdk = 33
 
-    const val agp = "8.1.0-beta01"
+    const val agp = "8.1.0-rc01"
     const val androidxViewModel = "2.6.1"
-    const val apollo = "3.8.1"
-    const val coil = "2.1.0"
-    const val composeBom = "2023.03.00"
-    const val composeCompiler = "1.4.6"
-    const val composeNavigation = "2.5.3"
-    const val kmmViewModel = "1.0.0-ALPHA-6"
-    const val kmpNativeCoroutines = "1.0.0-ALPHA-7"
-    const val koin = "3.4.0"
-    const val koinCompose = "3.4.4"
-    const val ksp = "1.8.20-1.0.11"
-    const val kotlin = "1.8.20"
+    const val apollo = "3.8.2"
+    const val coil = "2.4.0"
+    const val composeBom = "2023.06.01"
+    const val composeCompiler = "1.4.7"
+    const val composeNavigation = "2.6.0"
+    const val kmmViewModel = "1.0.0-ALPHA-9"
+    const val kmpNativeCoroutines = "1.0.0-ALPHA-10"
+    const val koin = "3.4.1"
+    const val koinCompose = "3.4.5"
+    const val ksp = "1.8.21-1.0.11"
+    const val kotlin = "1.8.21"
     const val timber = "5.0.1"
 }
 
